3ff507f7bae99aac109aa949b00d4844a4c3889f
Asisha351 <asisha.syed@gmail.com>
2026-03-23 23:17:19 +0530

    log

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from "react";
 
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
-function App() {
-  const[activePlayer, setActivePlayer] = useState("X");
+import Log from "./components/Log.jsx";
 
-  function handlePlayerChange(){
+function App() {
+  const[gameTurns, setGameTurns] = useState([]); //🔴this will declare an array
+  //const[activePlayer, setActivePlayer] = useState("X"); //u used better optimised approach
+
+  let currentPlayer='X'; 
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  
+
+  function handlePlayerChange(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+    setGameTurns(prevTurns => {
+
+      let currentPlayer='X'; //🔴we dont use activePlayer as it is not updated immediately in react
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+
+      //🔴Use state only for data that changes AND affects UI — everything else should be derived.
+      const updatedTurns = [
+        //🔴Adds a new move at the start of array, object with properties
+        { square : {row: rowIndex, col: colIndex}, player: currentPlayer },
+          ...prevTurns,
+      ]; //
+      return updatedTurns;
+
+    });
   }
 
   return (
@@ -16,8 +41,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer==='X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'} />
         </ol>
-        <GameBoard onSelectSquare={handlePlayerChange} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handlePlayerChange} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} /> //🔴we pass gameTurns state as props to another component, we can pass state as prop and it renders the log of moves
     </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 
 //matrix of 3x3 with i=0,j=3
 const intialGameboard = [
@@ -8,31 +6,45 @@ const intialGameboard = [
     [null, null, null]
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const[gameboard, setGameboard] = useState(intialGameboard);
+export default function GameBoard({onSelectSquare, turns}) {
+    
+    // 🟢METHOD 1 : storing gameboard in a state 
+    // const[gameboard, setGameboard] = useState(intialGameboard);
 
-    function handleSelectSquare(rowIndex, colIndex){
-        setGameboard((prevGameboard) => {
-            const updatedGameBoard = [...prevGameboard.map(innerArray => [...innerArray] )];
-            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedGameBoard;
-        });
-        onSelectSquare();
+    // function handleSelectSquare(rowIndex, colIndex){
+    //     setGameboard((prevGameboard) => {
+    //         const updatedGameBoard = [...prevGameboard.map(innerArray => [...innerArray] )];
+    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedGameBoard;
+    //     });
+    //     onSelectSquare();
+    // }
+    
+    // 🟢METHOD 2 : deriving gameboard from turns array state (no state for gameboard)
+    //u should try to optimise no of staes used
+    let gameboard = intialGameboard;
+    for(const turn of turns){
+        const {square, player} = turn;
+        const{row, col} = square; //deconstructing
+
+        gameboard[row][col] = player;
     }
+    
+    //🔴make notes about parent re-render in chatgpt
 
     //map function iterates through the gameboard array, takes each element as inout 
     //to function and returns a new array with the results of calling the function on each element.
     return (
         <ol id="game-board">
-            {gameboard.map((row, rowIndex) => ( //gameboard[rowIndex] = row , only outer array is mapped
-
+            {gameboard.map((row, rowIndex) => ( 
+            //gameboard[rowIndex] = row , only outer array is mapped
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => ( //row[colIndex] = playerSymbol, only inner array is mapped
 
                             //Each square is another list item.
                             <li key={colIndex}> 
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                                     {playerSymbol}
                                 </button>
                             </li>
